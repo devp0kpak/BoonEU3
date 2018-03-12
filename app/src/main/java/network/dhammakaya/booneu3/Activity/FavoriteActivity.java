bd74a7048d9498c0e143e799d2c8f103a5bde260
Puttapan <devp0kpak@gmail.com>
2018-03-12 18:32:50 +0700

    add splash screen add login activity add Design Detail

--- app/src/main/java/network/dhammakaya/booneu3/Activity/FavoriteActivity.java
+++ app/src/main/java/network/dhammakaya/booneu3/Activity/FavoriteActivity.java
@@ -2,14 +2,39 @@ package network.dhammakaya.booneu3.Activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import network.dhammakaya.booneu3.R;
 
-public class FavoriteActivity extends Activity {
+public class FavoriteActivity extends Activity implements View.OnClickListener {
+
+    private ImageView iv_btn_back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favorite);
+
+        initView();
+        initListener();
+    }
+
+    private void initView() {
+        iv_btn_back = (ImageView) findViewById(R.id.iv_btn_back);
+    }
+
+    private void initListener() {
+
+        iv_btn_back.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if (v == iv_btn_back) {
+            finish();
+        }
     }
 }
--- app/src/main/java/network/dhammakaya/booneu3/Activity/MainActivity.java
+++ app/src/main/java/network/dhammakaya/booneu3/Activity/MainActivity.java
@@ -4,8 +4,13 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.design.widget.BottomSheetBehavior;
+import android.support.design.widget.BottomSheetDialog;
+import android.text.Layout;
+import android.util.TypedValue;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.prolificinteractive.materialcalendarview.CalendarDay;
@@ -26,9 +31,18 @@ public class MainActivity extends Activity implements View.OnClickListener{
     private MaterialCalendarView mcv;
 
     private ImageView iv_setting;
+    private ImageView iv_btn_favorite;
     private CustomTextView tv_day;
     private CustomTextView tv_month;
     private CustomTextView tv_year;
+
+    private BottomSheetDialog bottomSheetDialog;
+
+    private View item_event;
+
+    //Bottom Sheet
+    private CustomTextView item_setting;
+    private CustomTextView item_line_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +50,7 @@ public class MainActivity extends Activity implements View.OnClickListener{
         setContentView(R.layout.activity_main);
 
         initView();
+        setBottomSheet();
         initListener();
         customCalendar();
 
@@ -72,24 +87,57 @@ public class MainActivity extends Activity implements View.OnClickListener{
         mcv = (MaterialCalendarView) findViewById(R.id.calendarView);
 
         iv_setting = (ImageView) findViewById(R.id.iv_setting);
+        iv_btn_favorite = (ImageView) findViewById(R.id.iv_btn_favorite);
         tv_day = (CustomTextView) findViewById(R.id.tv_day);
         tv_month = (CustomTextView) findViewById(R.id.tv_month);
         tv_year = (CustomTextView) findViewById(R.id.tv_year);
+
+        item_event = (View) findViewById(R.id.item_event);
     }
 
     private void initListener() {
 
         iv_setting.setOnClickListener(this);
+        iv_btn_favorite.setOnClickListener(this);
+        item_event.setOnClickListener(this);
+        item_setting.setOnClickListener(this);
+        item_line_login.setOnClickListener(this);
 
     }
 
     @Override
     public void onClick(View v) {
 
-        if(v == iv_setting){
+        if (v == iv_setting){
+            bottomSheetDialog.show();
+        }
+
+        if (v == iv_btn_favorite) {
+            Intent layoutFavorite = new Intent(this, FavoriteActivity.class);
+            startActivity(layoutFavorite);
+        }
+
+        if (v == item_event) {
+            Intent layoutDetail = new Intent(this, DetailActivity.class);
+            startActivity(layoutDetail);
+        }
+
+        if (v == item_setting) {
+            bottomSheetDialog.dismiss();
             Intent layoutSetting = new Intent(this, SettingActivity.class);
             startActivity(layoutSetting);
         }
 
+    }
+
+    private void setBottomSheet() {
+        bottomSheetDialog = new BottomSheetDialog(MainActivity.this);
+        View bottomSheetView = getLayoutInflater().inflate(R.layout.view_bottomsheet_menu, null);
+        bottomSheetDialog.setContentView(bottomSheetView);
+        BottomSheetBehavior bottomSheetBehavior = BottomSheetBehavior.from((View) bottomSheetView.getParent());
+        bottomSheetBehavior.setPeekHeight((int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 300, getResources().getDisplayMetrics()));
+
+        item_setting = (CustomTextView) bottomSheetView.findViewById(R.id.item_setting);
+        item_line_login = (CustomTextView) bottomSheetView.findViewById(R.id.item_line_login);
     }
 }
