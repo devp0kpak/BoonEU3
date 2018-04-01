e2ad3df48ecc9313344823c38c916b0303f98a5d
Puttapan <devp0kpak@gmail.com>
2018-04-01 18:56:44 +0700

    add View Image

---
+++ app/src/main/java/network/dhammakaya/booneu3/Activity/ViewImageActivity.java
@@ -0,0 +1,35 @@
+package network.dhammakaya.booneu3.Activity;
+
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.github.chrisbanes.photoview.PhotoView;
+
+import network.dhammakaya.booneu3.R;
+
+public class ViewImageActivity extends AppCompatActivity {
+    private ImageView btnBack;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_image);
+
+        btnBack = (ImageView) findViewById(R.id.btnBack);
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        String mImageUri = getIntent().getExtras().getString("imageUri");
+
+        PhotoView photoView = (PhotoView) findViewById(R.id.photo_view);
+        photoView.setImageURI(Uri.parse(mImageUri));
+
+    }
+}
